444b938cc6a08541414f1c0e0d513307b95af1e5
Ry Racherbaumer <ry@awakenetworks.com>
2016-06-07 14:22:42 -0700

    Update bootstrap command (fixes #11, #176)
    * Symlink all packages to root node_modules folder after installing external dependencies
    * Find common external dependencies and install to root node_modules folder
    * Symlink all binaries to packages that depend on them
    * Add `symlink` method to FileSystemUtilities
    * Move `hasMatchingDependency` and `hasDependencyInstalled` to Package
    * Refactor bootstrap unit tests
    * Add bootstrap unit test when symlinking binaries
    * Add unit test for `Package.hasMatchingDependency`

--- src/commands/BootstrapCommand.js
+++ src/commands/BootstrapCommand.js
@@ -14,147 +14,345 @@ export default class BootstrapCommand extends Command {
   }
 
   execute(callback) {
-    this.linkDependencies(err => {
+    this.bootstrapPackages(err => {
       if (err) {
         callback(err);
       } else {
-        this.logger.success("Successfully bootstrapped " + this.packages.length + " packages.");
+        this.logger.success(`Successfully bootstrapped ${this.packages.length} packages.`);
         callback(null, true);
       }
     });
   }
 
-  linkDependencies(callback) {
-    this.progressBar.init(this.packages.length);
-    this.logger.info("Linking all dependencies");
+  /**
+   * Return packages that satisfy the dependency requirements of the passed package
+   * @param {Package} pkg The package from which to check dependencies
+   * @returns {Array.<Package>}
+   */
+  getMatchedDependencies(pkg) {
+    return this.packages.filter(dependency => pkg.hasMatchingDependency(dependency, true));
+  }
 
+  /**
+   * Bootstrap packages
+   * @param {Function} callback
+   */
+  bootstrapPackages(callback) {
+    this.packages = this.getPackages();
+    this.logger.info(`Bootstrapping ${this.packages.length} packages`);
+    async.series(this.getBootstrapActions(), callback);
+  }
+
+  getPackages() {
     const ignore = this.flags.ignore || this.repository.bootstrapConfig.ignore;
-
-    async.parallelLimit(PackageUtilities.filterPackages(this.packages, ignore, true).map(pkg => done => {
-      async.series([
-        cb => FileSystemUtilities.mkdirp(pkg.nodeModulesLocation, cb),
-        cb => this.installExternalPackages(pkg, cb),
-        cb => this.linkDependenciesForPackage(pkg, cb)
-      ], err => {
-        this.progressBar.tick(pkg.name);
-        done(err);
-      });
-    }), this.concurrency, err => {
-      this.progressBar.terminate();
-      callback(err);
-    });
+    if (ignore) {
+      this.logger.info(`Ignoring packages that match '${ignore}'`);
+    }
+    return PackageUtilities.filterPackages(this.packages, ignore, ignore ? true : false);
   }
 
-  linkDependenciesForPackage(pkg, callback) {
-    async.each(this.packages, (dependency, done) => {
-      if (!this.hasMatchingDependency(pkg, dependency, true)) return done();
-
-      const linkSrc = dependency.location;
-      const linkDest = path.join(pkg.nodeModulesLocation, dependency.name);
-
-      this.createLinkedDependency(linkSrc, linkDest, dependency.name, done);
-    }, callback);
-  }
-
-  createLinkedDependency(src, dest, name, callback) {
-    FileSystemUtilities.rimraf(dest, err => {
-      if (err) {
-        return callback(err);
-      }
-
-      FileSystemUtilities.mkdirp(dest, err => {
-        if (err) {
-          return callback(err);
+  /**
+   * Returns an array of async actions to execute during bootstrap
+   * @returns {Array}
+   */
+  getBootstrapActions() {
+    // async actions to perform during bootstrap
+    const actions = [];
+    const packagesWithBins = [];
+    // for each package to bootstrap
+    this.packages.forEach(pkg => {
+      // array of dependencies with binaries
+      const depsWithBin = [];
+      // for each matched dependency
+      this.getMatchedDependencies(pkg).forEach(dependency => {
+        if (dependency.bin) {
+          depsWithBin.push(dependency);
         }
-
-        this.createLinkedDependencyFiles(src, dest, name, callback);
       });
-    });
-  }
-
-  createLinkedDependencyFiles(src, dest, name, callback) {
-    const srcPackageJsonLocation = path.join(src, "package.json");
-    const destPackageJsonLocation = path.join(dest, "package.json");
-    const destIndexJsLocation = path.join(dest, "index.js");
-
-    const packageJsonFileContents = JSON.stringify({
-      name: name,
-      version: require(srcPackageJsonLocation).version
-    }, null, "  ");
-
-    const prefix = this.repository.linkedFiles.prefix || "";
-    const indexJsFileContents = prefix + "module.exports = require(" + JSON.stringify(src) + ");";
-
-    FileSystemUtilities.writeFile(destPackageJsonLocation, packageJsonFileContents, err => {
-      if (err) {
-        return callback(err);
+      // if package has dependencies with binaries
+      if (depsWithBin.length) {
+        packagesWithBins.push([pkg, depsWithBin]);
       }
-
-      FileSystemUtilities.writeFile(destIndexJsLocation, indexJsFileContents, callback);
     });
+    // symlink dependency binaries for all packages
+    actions.push(cb => this.symlinkPackageBinaries(packagesWithBins, cb))
+    const depsToInstall = this.getDependenciesToInstall(this.packages);
+    // install external dependencies (do this first)
+    actions.unshift(cb => this.installExternalDependencies(depsToInstall, cb));
+    // if installing dependencies at the root
+    if (depsToInstall.__ROOT__.length) {
+      // get dependenciy names
+      const dependencies = depsToInstall.__ROOT__.map(name => name.split("@")[0]);
+      // symlink binaries to each package
+      actions.push(cb => this.symlinkDependencyBinaries(dependencies, cb));
+    }
+    // symlink packages at the root
+    actions.push(cb => this.symlinkPackages(cb));
+    return actions;
   }
 
-  installExternalPackages(pkg, callback) {
-    const allDependencies = pkg.allDependencies;
-
-    const externalPackages = Object.keys(allDependencies)
-      .filter(dependency => {
-        const match = find(this.packages, pkg => {
-          return pkg.name === dependency;
-        });
-
-        return !(match && this.hasMatchingDependency(pkg, match));
-      })
-      .filter(dependency => {
-        return !this.hasDependencyInstalled(pkg, dependency);
-      })
-      .map(dependency => {
-        return dependency + "@" + allDependencies[dependency];
+  /**
+   * Symlink package dependency binaries to the package's node_modules/.bin folder
+   * @param {Array} packages
+   * @param {Function} callback
+   */
+  symlinkPackageBinaries(packages, callback) {
+    const actions = [];
+    this.logger.info("Symlinking binaries for packages");
+    packages.forEach(pkgWithBin => {
+      const [pkg, dependencies] = pkgWithBin;
+      dependencies.forEach(dependency => {
+        const { location, name } = dependency;
+        const destPath = path.join(pkg.nodeModulesLocation);
+        actions.push(cb => this.createBinaryLink(location, destPath, name, dependency.bin, cb));
       });
-
-    if (externalPackages.length) {
-      NpmUtilities.installInDir(pkg.location, externalPackages, callback);
-    } else {
-      callback();
-    }
+    });
+    async.parallelLimit(actions, this.concurrency, callback);
   }
 
-  hasMatchingDependency(pkg, dependency, showWarning = false) {
-    const expectedVersion = pkg.allDependencies[dependency.name];
-    const actualVersion = dependency.version;
-
-    if (!expectedVersion) {
-      return false;
-    }
-
-    if (this.isCompatableVersion(actualVersion, expectedVersion)) {
-      return true;
-    }
-
-    if (showWarning) {
-      this.logger.warning(
-        `Version mismatch inside "${pkg.name}". ` +
-        `Depends on "${dependency.name}@${expectedVersion}" ` +
-        `instead of "${dependency.name}@${actualVersion}".`
-      );
-    }
-
-    return false;
+  /**
+   * Create a symlink to a dependency's binary in the node_modules/.bin folder
+   * @param {String} src
+   * @param {String} dest
+   * @param {String} name
+   * @param {String|Object} bin
+   * @param {Function} callback
+   */
+  createBinaryLink(src, dest, name, bin, callback) {
+    const destBinFolder = path.join(dest, ".bin");
+    const bins = typeof bin === "string" ?
+      { [name]: bin } :
+      bin;
+    const srcBinFiles = Object.keys(bins).map(name => path.join(src, bins[name]));
+    const destBinFiles = Object.keys(bins).map(name => path.join(destBinFolder, name));
+    const actions = [cb => FileSystemUtilities.mkdirp(destBinFolder, cb)];
+    srcBinFiles.forEach((binFile, idx) => {
+      actions.push(cb => FileSystemUtilities.symlink(binFile, destBinFiles[idx], cb));
+    });
+    async.series(actions, callback);
   }
 
-  hasDependencyInstalled(pkg, dependency) {
-    const packageJson = path.join(pkg.nodeModulesLocation, dependency, "package.json");
+  /**
+   * Given an array of packages, return map of dependencies to install
+   * @param {Array.<Package>} packages An array of packages
+   * @returns {Object}
+   */
+  getDependenciesToInstall(packages = []) {
+    // find package by name
+    const findPackage = (name, version) => find(this.packages, pkg => {
+      return pkg.name === name && (!version || semver.satisfies(pkg.version, version));
+    });
+    const hasPackage = (name, version) => Boolean(findPackage(name, version));
+    /**
+     * Map of dependency install locations
+     *   - keys represent a package name (i.e. "my-component")
+     *     "__ROOT__" is a special value and refers to the root folder
+     *   - values are an array of strings representing the dependency and its version to install
+     *     (i.e. ["react@15.x", "react-dom@^15.0.0", "webpack@~1.13.0"]
+     *
+     * {
+     *   <package>: [<dependency1@version>, <dependency2@version>, ...]
+     * }
+     */
+    const installs = { __ROOT__: [] };
+    /**
+     * Map of dependencies to install
+     * {
+     *   <name>: {
+     *     versions: {
+     *       <version>: <# of dependents>
+     *     },
+     *     dependents: {
+     *       <version>: [<dependent1>, <dependent2>, ...]
+     *     }
+     *   }
+     * }
+     *
+     * Example:
+     *
+     * {
+     *   react: {
+     *     versions: {
+     *       "15.x": 3,
+     *       "^0.14.0": 1
+     *     },
+     *     dependents: {
+     *       "15.x": ["my-component1", "my-component2", "my-component3"],
+     *       "^0.14.0": ["my-component4"],
+     *     }
+     *   }
+     * }
+     */
+    const depsToInstall = {};
+    // get the map of external dependencies to install
+    packages.forEach(pkg => {
+      // for all package dependencies
+      Object.keys(pkg.allDependencies)
+        // map to package or normalized external dependency
+        .map(name => findPackage(name, pkg.allDependencies[name]) || { name, version: pkg.allDependencies[name] })
+        // match external and version mismatched local packages
+        .filter(dep => !hasPackage(dep.name, dep.version) || !pkg.hasMatchingDependency(dep))
+        .forEach(dep => {
+          const { name, version } = dep;
+          if (!depsToInstall[name]) {
+            depsToInstall[name] = {
+              versions: {},
+              dependents: {}
+            };
+          }
+          // add dependency version
+          if (!depsToInstall[name].versions[version]) {
+            depsToInstall[name].versions[version] = 1;
+          } else {
+            depsToInstall[name].versions[version]++;
+          }
+          // add package with required version
+          if (!depsToInstall[name].dependents[version]) {
+            depsToInstall[name].dependents[version] = [];
+          }
+          depsToInstall[name].dependents[version].push(pkg.name);
+        });
+    });
+    // determine where each dependency will be installed
+    Object.keys(depsToInstall).forEach(name => {
+      const allVersions = Object.keys(depsToInstall[name].versions);
+      // create an object whose keys are the number of dependents
+      // with values that are the version those dependents need
+      const reversedVersions = Object.keys(depsToInstall[name].versions).reduce((versions, version) => {
+        versions[depsToInstall[name].versions[version]] = version;
+        return versions;
+      }, {});
+      // get the most common version
+      const max = Math.max.apply(null, Object.keys(reversedVersions).map(v => parseInt(v, 10)));
+      const commonVersion = reversedVersions[max.toString()];
+      // get an array of packages that depend on this external module
+      const deps = depsToInstall[name].dependents[commonVersion];
+      // check if the external dependency is not a package with a version mismatch,
+      // and is not already installed at root
+      if (!hasPackage(name) && !this.dependencySatisfiesPackages(name, deps.map(dep => findPackage(dep)))) {
+        // add the common version to root install
+        installs.__ROOT__.push(`${name}@${commonVersion}`);
+      }
+      // add less common versions to package installs
+      allVersions.forEach(version => {
+        // only install less common versions,
+        // unless it's a version-mismatched package
+        if (version !== commonVersion || hasPackage(name)) {
+          depsToInstall[name].dependents[version].forEach(pkg => {
+            // only install dependency if it's not already installed
+            if (!findPackage(pkg).hasDependencyInstalled(name)) {
+              if (!installs[pkg]) {
+                installs[pkg] = [];
+              }
+              installs[pkg].push(`${name}@${version}`);
+              this.logger.warning(
+                `"${pkg}" package depends on ${name}@${version}, ` +
+                `which differs from the more common ${name}@${commonVersion}.`
+              );
+            }
+          });
+        }
+      });
+    });
+    return installs;
+  }
+
+  /**
+   * Determine if a dependency installed at the root satifies the requirements of the passed packages
+   * This helps to optimize the bootstrap process and skip dependencies that are already installed
+   * @param {String} dependency
+   * @param {Array.<String>} packages
+   */
+  dependencySatisfiesPackages(dependency, packages) {
+    const packageJson = path.join(this.repository.rootPath, "node_modules", dependency, "package.json");
     try {
-      return this.isCompatableVersion(
-        require(packageJson).version,
-        pkg.allDependencies[dependency]
-      );
+      return packages.every(pkg => {
+        return semver.satisfies(
+          require(packageJson).version,
+          pkg.allDependencies[dependency]
+        );
+      });
     } catch (e) {
       return false;
     }
   }
 
-  isCompatableVersion(actual, expected) {
-    return semver.satisfies(actual, expected);
+  /**
+   * Install external dependencies for packages
+   * @param {Object} dependencies
+   * @param {Function} callback
+   */
+  installExternalDependencies(dependencies, callback) {
+    const actions = [];
+    Object.keys(dependencies)
+      .forEach(dest => {
+        const destLocation = dest === "__ROOT__" ?
+          this.repository.rootPath :
+          path.join(this.repository.packagesLocation, dest);
+        if (dependencies[dest].length) {
+          actions.push(cb => NpmUtilities.installInDir(destLocation, dependencies[dest], cb));
+        }
+      });
+    if (actions.length) {
+      const totalDependencies = Object.keys(dependencies)
+        .reduce((count, pkg) => dependencies[pkg].length + count, 0);
+      this.logger.info(`Installing ${totalDependencies} external dependencies`);
+    }
+    async.parallelLimit(actions, this.concurrency, callback);
+  }
+
+  /**
+   * Symlink binaries of modules installed at the root
+   * @param {Array.<String>} dependencies
+   * @param {Function} callback
+   */
+  symlinkDependencyBinaries(dependencies = [], callback) {
+    const actions = [];
+    let binCount = 0;
+    dependencies.forEach(dependency => {
+      const packageLocation = path.join(this.repository.rootPath, "node_modules", dependency);
+      const packageJsonLocation = path.join(packageLocation, "package.json");
+      if (!FileSystemUtilities.existsSync(packageJsonLocation)) {
+        this.logger.error(`Unable to find package.json for ${dependency} dependency`);
+      } else {
+        const packageJson = require(packageJsonLocation);
+        if (packageJson.bin) {
+          binCount += typeof packageJson.bin === "string" ?
+            1 :
+            Object.keys(packageJson.bin).length;
+          // create a binary link to this package's bin file for each local package
+          this.packages.forEach(localPkg => {
+            const destPath = path.join(localPkg.nodeModulesLocation);
+            // skip binary link if dependency was installed locally
+            if (!FileSystemUtilities.existsSync(path.join(destPath, dependency))) {
+              actions.push(cb => this.createBinaryLink(packageLocation, destPath, dependency, packageJson.bin, cb));
+            }
+          });
+        }
+      }
+    });
+    this.logger.info(`Symlinking ${binCount} external dependency binaries`);
+    async.parallelLimit(actions, this.concurrency, callback);
+  }
+
+  /**
+   * Symlink packages to the root node_modules folder
+   * @param {Function} callback
+   */
+  symlinkPackages(callback) {
+    this.logger.info("Symlinking packages to root");
+    const destFolder = path.join(this.repository.rootPath, "node_modules");
+    const actions = [cb => FileSystemUtilities.mkdirp(destFolder, cb)];
+    this.packages.forEach(pkg => {
+      const srcPackageLocation = path.join(this.repository.packagesLocation, pkg.name);
+      const destPackageLink = path.join(destFolder, pkg.name);
+      actions.push(cb => FileSystemUtilities.symlink(srcPackageLocation, destPackageLink, cb, "dir"));
+      if (pkg.bin) {
+        const { location, name } = pkg;
+        const destPath = path.join(this.repository.rootPath, "node_modules");
+        actions.push(cb => this.createBinaryLink(location, destPath, name, pkg.bin, cb));
+      }
+    });
+    async.parallelLimit(actions, this.concurrency, callback);
   }
 }
--- test/BootstrapCommand.js
+++ test/BootstrapCommand.js
@@ -20,62 +20,27 @@ describe("BootstrapCommand", () => {
       testDir = initFixture("BootstrapCommand/basic", done);
     });
 
-    it("should bootstrap files", done => {
+    it("should bootstrap packages", done => {
       const bootstrapCommand = new BootstrapCommand([], {});
 
       bootstrapCommand.runValidations();
       bootstrapCommand.runPreparations();
 
-      assertStubbedCalls([
-        [ChildProcessUtilities, "spawn", { nodeCallback: true }, [
-          { args: ["npm", ["install", "package-1@^0.0.0"], { cwd: path.join(testDir, "packages/package-4"), stdio: "ignore" }] }
-        ]]
-      ]);
+      const depsToInstall = bootstrapCommand.getDependenciesToInstall(bootstrapCommand.getPackages());
 
-      bootstrapCommand.runCommand(exitWithCode(0, err => {
-        if (err) return done(err);
-
-        try {
-          assert.ok(!pathExists.sync(path.join(testDir, "lerna-debug.log")), "lerna-debug.log should not exist");
-
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-1/node_modules")));
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-2/node_modules")));
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-3/node_modules")));
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-4/node_modules")));
-
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-2/node_modules/package-1")));
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-2/node_modules/package-1/index.js")));
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-2/node_modules/package-1/package.json")));
-
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-3/node_modules/package-2")));
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-3/node_modules/package-2/index.js")));
-          assert.ok(pathExists.sync(path.join(testDir, "packages/package-3/node_modules/package-2/package.json")));
-
-          // Should not exist because mis-matched version
-          assert.ok(!pathExists.sync(path.join(testDir, "packages/package-4/node_modules/package-1")));
-
-          assert.equal(fs.readFileSync(path.join(testDir, "packages/package-2/node_modules/package-1/index.js")).toString(), "/**\n * @prefix\n */\nmodule.exports = require(\"" + path.join(testDir, "packages/package-1") + "\");\n");
-          assert.equal(fs.readFileSync(path.join(testDir, "packages/package-2/node_modules/package-1/package.json")).toString(), "{\n  \"name\": \"package-1\",\n  \"version\": \"1.0.0\"\n}\n");
-
-          assert.equal(fs.readFileSync(path.join(testDir, "packages/package-3/node_modules/package-2/index.js")).toString(), "/**\n * @prefix\n */\nmodule.exports = require(\"" + path.join(testDir, "packages/package-2") + "\");\n");
-          assert.equal(fs.readFileSync(path.join(testDir, "packages/package-3/node_modules/package-2/package.json")).toString(), "{\n  \"name\": \"package-2\",\n  \"version\": \"1.0.0\"\n}\n");
-
-          done();
-        } catch (err) {
-          done(err);
-        }
-      }));
-    });
-
-    it("should not bootstrap an ignored package", done => {
-      const bootstrapCommand = new BootstrapCommand([], {
-        ignore: "package-2"
+      assert.deepEqual(depsToInstall, {
+        __ROOT__: ["foo@^1.0.0"],
+        "package-3": ["foo@0.1.12"],
+        "package-4": ["package-1@^0.0.0"]
       });
 
-      bootstrapCommand.runValidations();
-      bootstrapCommand.runPreparations();
-
       assertStubbedCalls([
+        [ChildProcessUtilities, "spawn", { nodeCallback: true }, [
+          { args: ["npm", ["install", "foo@^1.0.0"], { cwd: testDir, stdio: "ignore" }] }
+        ]],
+        [ChildProcessUtilities, "spawn", { nodeCallback: true }, [
+          { args: ["npm", ["install", "foo@0.1.12"], { cwd: path.join(testDir, "packages/package-3"), stdio: "ignore" }] }
+        ]],
         [ChildProcessUtilities, "spawn", { nodeCallback: true }, [
           { args: ["npm", ["install", "package-1@^0.0.0"], { cwd: path.join(testDir, "packages/package-4"), stdio: "ignore" }] }
         ]]
@@ -86,7 +51,26 @@ describe("BootstrapCommand", () => {
 
         try {
           assert.ok(!pathExists.sync(path.join(testDir, "lerna-debug.log")), "lerna-debug.log should not exist");
-          assert.ok(!pathExists.sync(path.join(testDir, "packages/package-2/node_modules/package-1")));
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules", "package-1")),
+            path.join(testDir, "packages/package-1"),
+            "package-1 should be symlinked"
+          );
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules", "package-2")),
+            path.join(testDir, "packages/package-2"),
+            "package-2 should be symlinked"
+          );
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules", "package-3")),
+            path.join(testDir, "packages/package-3"),
+            "package-3 should be symlinked"
+          );
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules", "package-4")),
+            path.join(testDir, "packages/package-4"),
+            "package-4 should be symlinked"
+          );
           done();
         } catch (err) {
           done(err);
@@ -102,12 +86,29 @@ describe("BootstrapCommand", () => {
       bootstrapCommand.runValidations();
       bootstrapCommand.runPreparations();
 
+      assertStubbedCalls([
+        [ChildProcessUtilities, "spawn", { nodeCallback: true }, [
+          { args: ["npm", ["install", "foo@^1.0.0"], { cwd: testDir, stdio: "ignore" }] }
+        ]]
+      ]);
+
       bootstrapCommand.runCommand(exitWithCode(0, err => {
         if (err) return done(err);
 
         try {
           assert.ok(!pathExists.sync(path.join(testDir, "lerna-debug.log")), "lerna-debug.log should not exist");
-          assert.ok(!pathExists.sync(path.join(testDir, "packages/package-3/node_modules/package-2")));
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules", "package-1")),
+            path.join(testDir, "packages/package-1"),
+            "package-1 should be symlinked"
+          );
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules", "package-2")),
+            path.join(testDir, "packages/package-2"),
+            "package-2 should be symlinked"
+          );
+          assert.throws(() => fs.readlinkSync(path.join(testDir, "node_modules", "package-3")));
+          assert.throws(() => fs.readlinkSync(path.join(testDir, "node_modules", "package-4")));
           done();
         } catch (err) {
           done(err);
@@ -129,11 +130,14 @@ describe("BootstrapCommand", () => {
       bootstrapCommand.runValidations();
       bootstrapCommand.runPreparations();
 
-      let installed = false;
+      let installed = 0;
+      let spawnArgs = [];
+      let spawnOptions = [];
+
       stub(ChildProcessUtilities, "spawn", (command, args, options, callback) => {
-        assert.deepEqual(args, ["install", "external@^1.0.0"])
-        assert.deepEqual(options, { cwd: path.join(testDir, "packages/package-1"), stdio: "ignore" })
-        installed = true;
+        spawnArgs.push(args);
+        spawnOptions.push(options);
+        installed++;
         callback();
       });
 
@@ -142,7 +146,15 @@ describe("BootstrapCommand", () => {
 
         try {
           assert.ok(!pathExists.sync(path.join(testDir, "lerna-debug.log")), "lerna-debug.log should not exist");
-          assert.ok(installed, "The external dependency was installed");
+          assert.deepEqual(spawnArgs, [
+            ["install", "external@^2.0.0"],
+            ["install", "external@^1.0.0"]
+          ]);
+          assert.deepEqual(spawnOptions, [
+            { cwd: testDir, stdio: "ignore" },
+            { cwd: path.join(testDir, "packages/package-1"), stdio: "ignore" }
+          ]);
+          assert.equal(installed, 2, "The external dependencies were installed");
 
           done();
         } catch (err) {
@@ -178,6 +190,58 @@ describe("BootstrapCommand", () => {
           assert.ok(!pathExists.sync(path.join(testDir, "lerna-debug.log")), "lerna-debug.log should not exist");
           assert.ok(!installed, "The external dependency was not installed");
 
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }));
+    });
+  });
+
+  describe("dependency binaries", () => {
+    let testDir;
+
+    beforeEach(done => {
+      testDir = initFixture("BootstrapCommand/bin", done);
+    });
+
+    it("should be linked in root and packages", done => {
+      const bootstrapCommand = new BootstrapCommand([], {});
+
+      bootstrapCommand.runValidations();
+      bootstrapCommand.runPreparations();
+
+      assertStubbedCalls([
+        [ChildProcessUtilities, "spawn", { nodeCallback: true }, [
+          { args: ["npm", ["install", "external@^1.0.0"], { cwd: path.join(testDir, "packages/package-1"), stdio: "ignore" }] }
+        ]]
+      ]);
+
+      bootstrapCommand.runCommand(exitWithCode(0, err => {
+        if (err) return done(err);
+
+        try {
+          assert.ok(!pathExists.sync(path.join(testDir, "lerna-debug.log")), "lerna-debug.log should not exist");
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules/.bin", "foo")),
+            path.join(testDir, "packages/package-2/foo.js"),
+            "foo binary from package-2 should be symlinked at the root"
+          );
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "node_modules/.bin", "bar")),
+            path.join(testDir, "packages/package-2/bar.js"),
+            "bar binary from package-2 should be symlinked at the root"
+          );
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "packages/package-3/node_modules/.bin/foo")),
+            path.join(testDir, "packages/package-2/foo.js"),
+            "foo binary package-2 should be symlinked in package-3"
+          );
+          assert.equal(
+            fs.readlinkSync(path.join(testDir, "packages/package-3/node_modules/.bin/bar")),
+            path.join(testDir, "packages/package-2/bar.js"),
+            "bar binary package-2 should be symlinked in package-3"
+          );
           done();
         } catch (err) {
           done(err);
